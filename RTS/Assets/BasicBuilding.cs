7bc43faf836ecf2241d3f41bd7d501545f18c8a7
jamie <jamiealexander.lobban@enti.cat>
2023-10-10 13:33:02 +0200

    jjj

--- RTS/Assets/BasicBuilding.cs
+++ RTS/Assets/BasicBuilding.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 namespace LP.FDG.Buildings
 {
@@ -13,18 +13,41 @@ namespace LP.FDG.Buildings
         public Transform attackPoint1;
         public Transform attackPoint2;
 
+
+        public float maxHealth, currentHealth;
+
+        [SerializeField] private Image healthBarAmount, healtBarBackground;
+
         private void Start()
         {
+            maxHealth = baseStats.health;
             attackPoint1 = gameObject.transform.GetChild(0);
             attackPoint2 = gameObject.transform.GetChild(1);
             // Register attack points with the AttackPointManager
             AttackPointManager.instance.availableAttackPoints.Add(attackPoint1);
             AttackPointManager.instance.availableAttackPoints.Add(attackPoint2);
+
+            healthBarAmount.enabled = false;
+            healtBarBackground.enabled = false;
         }
 
         private void Update()
         {
+            if(currentHealth <maxHealth)
+            {
 
+            }
+        }
+
+
+        void HandleHealthBuilding()
+        {
+
+            Camera camera = Camera.main;
+            gameObject.transform.LookAt(gameObject.transform.position + camera.transform.rotation* Vector3.forward,
+                camera.transform.rotation* Vector3.up);
+
+            healthBarAmount.fillAmount = currentHealth / maxHealth;
         }
     }
 
